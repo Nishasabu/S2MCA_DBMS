a04d438ee615d8ac86783d7f79cb00b6c8bdab53
nishasabu <nishamariyasabu7@gmail.com>
2021-06-13 23:25:04 +0530

    2nd commit

--- Cycle2_Quset-3.sql
+++ Cycle2_Quset-3.sql
@@ -3,12 +3,15 @@ CREATE TABLE CUSTOMER
 cust_id   INT PRIMARY KEY,
 cust_name VARCHAR(20) NOT NULL
 );
+
+DESCRIBE TABLE CUSTOMER;
 CREATE TABLE ITEM
 (
 item_id INT PRIMARY KEY, 
 item_name VARCHAR(25), 
 price INT
 );
+DESCRIBE TABLE ITEM;
 
 CREATE TABLE SALE
 (
@@ -22,27 +25,25 @@ REFERENCES item(item_id),
 FOREIGN KEY (cust_id) 
 REFERENCES customer(cust_id)
   );
-  
-  INSERT INTO customer VALUES( 1, 'Rekha'),(2,'liya'),(3,'maria'),(4,'hello'),(5,'sani');
-   
-  INSERT INTO item VALUES( 1, 'Rusk', 120),(2,'banana',50),(3,'handwash',60),(4,'cake',420),(5,'sweets',25);
-
-  INSERT INTO sale VALUES ( 10, '2020-10-01', 1, 1, 3), ( 11, '2020-10-01', 1, 3, 2),
-   ( 12, '2020-10-01', 1, 5, 7),   ( 13, '2018-10-01', 4, 4, 1),  
-   ( 14, '2018-10-11', 4, 2, 2),  ( 15, '2018-09-29', 5, 1, 5),( 16, '2019-12-25', 3, 1, 5),( 17, '1995-06-21', 5, 4, 4),
-  ( 18,'2002-04-01', 4, 5, 5),( 19, '2020-02-12', 1, 2, 1);
-  
-  select * from sale;
-  
- 3. SELECT *FROM customer,item,sale WHERE price      > 200 AND sale.item_id = item.item_id AND 
- sale.cust_id = customer.cust_id;
- 4. SELECT C.cust_id,cust_name,SC.COUNT,SC.bill_date FROM CUSTOMER C,
-(SELECT COUNT(ITEM_ID) AS COUNT,bill_date,cust_id FROM SALE GROUP BY bill_date) SC WHERE C.cust_id=SC.cust_id;
-
-5 SELECT cust_name,  COUNT(item_id), bill_date  FROM customer c,  sale s WHERE c.cust_id = s.cust_id
- GROUP BY cust_name,  bill_date HAVING extract(YEAR FROM bill_date) = 2018;
-6 SELECT item_name FROM item,  sale WHERE sale.item_id = item.item_id AND sale.cust_id   = 5;
-7 
+DESCRIBE TABLE SALE;
+INSERT INTO CUSTOMER VALUES( 1, 'Rekha'),(2,'liya'),(3,'maria'),(4,'varun'),(5,'mini');
+SELECT * FROM CUSTOMER;
+INSERT INTO ITEM VALUES( 1, 'Rusk', 120),(2,'banana',50),(3,'handwash',60),(4,'cake',420),
+(5,'sweets',25);
+SELECT * FROM ITEM;
+INSERT INTO SALE VALUES ( 10, '2020-10-01', 1, 1, 3), ( 11, '2020-10-01', 1, 3, 2),
+( 12, '2020-10-01', 1, 5, 7),   ( 13, '2018-10-01', 4, 4, 1),  
+( 14, '2018-10-11', 4, 2, 2),  ( 15, '2018-09-29', 5, 1, 5),( 16, '2019-12-25', 3, 1, 5),
+( 17, '1995-06-21', 5, 4, 4),( 18,'2002-04-01', 4, 5, 5),( 19, '2020-02-12', 1, 2, 1);
+SELECT  * FROM SALE;
+SELECT *FROM CUSTOMER,SALE,ITEM WHERE price> 200 AND SALE.item_id = ITEM.item_id AND 
+SALE.cust_id = CUSTOMER.cust_id;
+SELECT C.cust_id,cust_name,SC.COUNT,SC.bill_date FROM CUSTOMER C,
+ (SELECT COUNT(ITEM_ID) AS COUNT,bill_date,cust_id FROM SALE GROUP BY bill_date) SC WHERE
+C.cust_id=SC.cust_id;
+SELECT cust_name,COUNT(item_id), bill_date  FROM CUSTOMER c,SALE s WHERE c.cust_id = s.cust_id
+GROUP BY cust_name,bill_date HAVING extract(YEAR FROM bill_date) = 2018;
+SELECT item_name FROM ITEM,SALE WHERE SALE.item_id = ITEM.item_id AND SALE.cust_id=5;
 SELECT item_name,S.bill_date FROM ITEM I,SALE S WHERE I.item_id=S.item_id AND S.bill_date=CURDATE();
-8 SELECT item_name,qty_sold,   price,(qty_sold * price) AS total_amount  FROM customer,   item,   sale
- WHERE cust_name  = 'Rekha' AND sale.item_id = item.item_id AND sale.cust_id = customer.cust_id;
+SELECT item_name,qty_sold,price,(qty_sold * price) AS total_amount  FROM CUSTOMER,SALE,ITEM
+WHERE cust_name  = 'Rekha' AND SALE.item_id = ITEM.item_id AND SALE.cust_id = CUSTOMER.cust_id;
